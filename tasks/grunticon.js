e270014c80822df7b86af5db818f99380e48d2ea
Jeffrey Lembeck <jlembeck@gmail.com>
2012-10-23 09:34:19 -0700

    There was a mixture of tabs and spaces and we prefer tabs

--- tasks/grunticon.js
+++ tasks/grunticon.js
@@ -8,94 +8,94 @@
 
 module.exports = function(grunt ) {
 
-  grunt.registerTask( 'grunticon', 'A mystical CSS icon solution.', function() {
+	grunt.registerTask( 'grunticon', 'A mystical CSS icon solution.', function() {
 
-    // just a quick starting message
-    grunt.log.write( "Look, it's a grunticon!\n" );
+		// just a quick starting message
+		grunt.log.write( "Look, it's a grunticon!\n" );
 
-    // get the config
-    var config = grunt.config.get( "grunticon" );
+		// get the config
+		var config = grunt.config.get( "grunticon" );
 
-    // fail if config or no src or dest config
-    if( !config || config.src === undefined || config.dest === undefined ){
-      grunt.fatal( "Oops! Please provide grunticon configuration for src and dest in your grunt.js file" );
-      return;
-    }
+		// fail if config or no src or dest config
+		if( !config || config.src === undefined || config.dest === undefined ){
+			grunt.fatal( "Oops! Please provide grunticon configuration for src and dest in your grunt.js file" );
+			return;
+		}
 
-    // make sure src and dest have / at the end
-    if( !config.src.match( /\/$/ ) ){
-        config.src += "/";
-    }
-    if( !config.dest.match( /\/$/ ) ){
-        config.dest += "/";
-    }
+		// make sure src and dest have / at the end
+		if( !config.src.match( /\/$/ ) ){
+				config.src += "/";
+		}
+		if( !config.dest.match( /\/$/ ) ){
+				config.dest += "/";
+		}
 
-    var asyncCSS = grunt.task.getFile( "grunticon/static/grunticon.loader.js" );
-    var asyncCSSBanner = grunt.task.getFile( "grunticon/static/grunticon.loader.banner.js" );
-    var previewHTMLsrc = grunt.task.getFile( "grunticon/static/preview.html" );
+		var asyncCSS = grunt.task.getFile( "grunticon/static/grunticon.loader.js" );
+		var asyncCSSBanner = grunt.task.getFile( "grunticon/static/grunticon.loader.banner.js" );
+		var previewHTMLsrc = grunt.task.getFile( "grunticon/static/preview.html" );
 
-    // CSS filenames with optional mixin from config
-    var datasvgcss = grunt.config.datasvgcss || "icons.data.svg.css";
-    var datapngcss = grunt.config.datapngcss || "icons.data.png.css";
-    var urlpngcss = grunt.config.urlpngcss || "icons.fallback.css";
+		// CSS filenames with optional mixin from config
+		var datasvgcss = grunt.config.datasvgcss || "icons.data.svg.css";
+		var datapngcss = grunt.config.datapngcss || "icons.data.png.css";
+		var urlpngcss = grunt.config.urlpngcss || "icons.fallback.css";
 
-    //filename for generated output preview HTML file
-    var previewhtml = config.previewhtml || "preview.html";
+		//filename for generated output preview HTML file
+		var previewhtml = config.previewhtml || "preview.html";
 
-    //filename for generated loader HTML snippet file
-    var loadersnippet = config.loadersnippet || "grunticon.loader.txt";
+		//filename for generated loader HTML snippet file
+		var loadersnippet = config.loadersnippet || "grunticon.loader.txt";
 
-    // css references base path for the loader
-    var cssbasepath = config.cssbasepath || "/";
+		// css references base path for the loader
+		var cssbasepath = config.cssbasepath || "/";
 
-    // folder name (within the output folder) for generated png files
-    var pngfolder = config.pngfolder || "png/";
-    // make sure pngfolder has / at the end
-    if( !pngfolder.match( /\/$/ ) ){
-        pngfolder += "/";
-    }
+		// folder name (within the output folder) for generated png files
+		var pngfolder = config.pngfolder || "png/";
+		// make sure pngfolder has / at the end
+		if( !pngfolder.match( /\/$/ ) ){
+				pngfolder += "/";
+		}
 
-    // css class prefix
-    var cssprefix = config.cssprefix || "icon";
-    
-    // create the output directory
-    grunt.file.mkdir( config.dest );
+		// css class prefix
+		var cssprefix = config.cssprefix || "icon";
 
-    // create the output icons directory
-    grunt.file.mkdir( config.dest + pngfolder );
+		// create the output directory
+		grunt.file.mkdir( config.dest );
 
-    // minify the source of the grunticon loader and write that to the output
-    grunt.log.write( "\ngrunticon now minifying the stylesheet loader source." );
-    var asyncsrc = grunt.file.read( asyncCSS );
-    var banner = grunt.file.read( asyncCSSBanner );
-    var min = banner + "\n" + grunt.helper('uglify', asyncsrc );
-    var loaderCodeDest = config.dest + loadersnippet;
-    grunt.file.write( loaderCodeDest, min );
-    grunt.log.write( "\ngrunticon loader file created." );
+		// create the output icons directory
+		grunt.file.mkdir( config.dest + pngfolder );
 
-    // take it to phantomjs to do the rest
-    grunt.log.write( "\ngrunticon now spawning phantomjs..." );
+		// minify the source of the grunticon loader and write that to the output
+		grunt.log.write( "\ngrunticon now minifying the stylesheet loader source." );
+		var asyncsrc = grunt.file.read( asyncCSS );
+		var banner = grunt.file.read( asyncCSSBanner );
+		var min = banner + "\n" + grunt.helper('uglify', asyncsrc );
+		var loaderCodeDest = config.dest + loadersnippet;
+		grunt.file.write( loaderCodeDest, min );
+		grunt.log.write( "\ngrunticon loader file created." );
 
-    grunt.utils.spawn({
-      cmd: 'phantomjs',
-      args: [
-        grunt.task.getFile('grunticon/phantom.js'),
-        config.src,
-        config.dest,
-        loaderCodeDest,
-        previewHTMLsrc,
-        datasvgcss,
-        datapngcss,
-        urlpngcss,
-        previewhtml,
-        pngfolder,
-        cssprefix,
-        cssbasepath
-      ],
-      fallback: ''
-    }, function(err, result, code) {
-      // TODO boost this up a bit.
-      grunt.log.write("\nSomething went wrong with phantomjs...");
-    });
-  });
+		// take it to phantomjs to do the rest
+		grunt.log.write( "\ngrunticon now spawning phantomjs..." );
+
+		grunt.utils.spawn({
+			cmd: 'phantomjs',
+			args: [
+				grunt.task.getFile('grunticon/phantom.js'),
+				config.src,
+				config.dest,
+				loaderCodeDest,
+				previewHTMLsrc,
+				datasvgcss,
+				datapngcss,
+				urlpngcss,
+				previewhtml,
+				pngfolder,
+				cssprefix,
+				cssbasepath
+			],
+			fallback: ''
+		}, function(err, result, code) {
+			// TODO boost this up a bit.
+			grunt.log.write("\nSomething went wrong with phantomjs...");
+		});
+	});
 };
--- tasks/grunticon/phantom.js
+++ tasks/grunticon/phantom.js
@@ -12,17 +12,17 @@
 
 /*
 phantom args sent from grunticon.js:
-  [0] - input directory path
-  [1] - output directory path
-  [2] - asyncCSS output file path
-  [3] - preview.html static file path
-  [4] - CSS filename for datasvg css
-  [5] - CSS filename for datapng css
-  [6] - CSS filename for urlpng css
-  [7] - filename for preview HTML file
-  [8] - png folder name
-  [9] - css classname prefix
-  [10] - css basepath prefix
+	[0] - input directory path
+	[1] - output directory path
+	[2] - asyncCSS output file path
+	[3] - preview.html static file path
+	[4] - CSS filename for datasvg css
+	[5] - CSS filename for datapng css
+	[6] - CSS filename for urlpng css
+	[7] - filename for preview HTML file
+	[8] - png folder name
+	[9] - css classname prefix
+	[10] - css basepath prefix
 */
 
 var fs = require( "fs" );
@@ -44,115 +44,115 @@ var cssbasepath = phantom.args[10];
 
 // increment the current file index and process it
 function nextFile(){
-  currfile++;
-  processFile();
+	currfile++;
+	processFile();
 }
 
 // files have all been processed. write the css and html files and return
 function finishUp(){
 
-  // make the preview HTML file and asyncCSS loader file
-  var asyncCSS = fs.read( phantom.args[2] );
+	// make the preview HTML file and asyncCSS loader file
+	var asyncCSS = fs.read( phantom.args[2] );
 
-  // copy above for a slightly different output in the preview html file (different paths)
-  var asyncCSSpreview = asyncCSS;
+	// copy above for a slightly different output in the preview html file (different paths)
+	var asyncCSSpreview = asyncCSS;
 
-  // open up the static html document
-  var htmldoc = fs.read( phantom.args[3]);
+	// open up the static html document
+	var htmldoc = fs.read( phantom.args[3]);
 
-  // noscript for the snippet file
-  var noscript = '<noscript><link href="' + cssbasepath + outputdir + fallbackcss + '" rel="stylesheet"></noscript>';
+	// noscript for the snippet file
+	var noscript = '<noscript><link href="' + cssbasepath + outputdir + fallbackcss + '" rel="stylesheet"></noscript>';
 
-  // noscript for the preview file
-  var noscriptpreview = '<noscript><link href="' + fallbackcss + '" rel="stylesheet"></noscript>';
+	// noscript for the preview file
+	var noscriptpreview = '<noscript><link href="' + fallbackcss + '" rel="stylesheet"></noscript>';
 
-  // add custom function call to asyncCSS
-  asyncCSS += '\ngrunticon( [ "' + cssbasepath + outputdir + datacss +'", "' + cssbasepath + outputdir + pngdatacss +'", "' + cssbasepath + outputdir + fallbackcss +'" ] );';
-  asyncCSSpreview += '\ngrunticon( [ "'+ datacss +'", "'+ pngdatacss +'", "'+ fallbackcss +'" ] );';
+	// add custom function call to asyncCSS
+	asyncCSS += '\ngrunticon( [ "' + cssbasepath + outputdir + datacss +'", "' + cssbasepath + outputdir + pngdatacss +'", "' + cssbasepath + outputdir + fallbackcss +'" ] );';
+	asyncCSSpreview += '\ngrunticon( [ "'+ datacss +'", "'+ pngdatacss +'", "'+ fallbackcss +'" ] );';
 
-  // add async loader to the top
-  htmldoc = htmldoc.replace( /<script>/, "<script>\n\t" + asyncCSSpreview );
+	// add async loader to the top
+	htmldoc = htmldoc.replace( /<script>/, "<script>\n\t" + asyncCSSpreview );
 
-  //add noscript
-  htmldoc = htmldoc.replace( /<\/script>/, "</script>\n\t" + noscriptpreview );
+	//add noscript
+	htmldoc = htmldoc.replace( /<\/script>/, "</script>\n\t" + noscriptpreview );
 
-  // add icons to the body
-  htmldoc = htmldoc.replace( /<\/body>/, htmlpreviewbody.join( "\n\t" ) + "\n</body>" );
+	// add icons to the body
+	htmldoc = htmldoc.replace( /<\/body>/, htmlpreviewbody.join( "\n\t" ) + "\n</body>" );
 
-  // write the preview html file
-  fs.write( outputdir + phantom.args[7], htmldoc );
+	// write the preview html file
+	fs.write( outputdir + phantom.args[7], htmldoc );
 
-  // write CSS files
-  fs.write( outputdir + fallbackcss, pngcssrules.join( "\n\n" ) );
-  fs.write( outputdir + pngdatacss, pngdatacssrules.join( "\n\n" ) );
-  fs.write( outputdir + datacss, datacssrules.join( "\n\n" ) );
+	// write CSS files
+	fs.write( outputdir + fallbackcss, pngcssrules.join( "\n\n" ) );
+	fs.write( outputdir + pngdatacss, pngdatacssrules.join( "\n\n" ) );
+	fs.write( outputdir + datacss, datacssrules.join( "\n\n" ) );
 
-  // overwrite the snippet HTML
-  fs.write( phantom.args[2], "<!-- Unicode CSS Loader: place this in the head of your page -->\n<script>\n" + asyncCSS + "</script>\n" + noscript );
+	// overwrite the snippet HTML
+	fs.write( phantom.args[2], "<!-- Unicode CSS Loader: place this in the head of your page -->\n<script>\n" + asyncCSS + "</script>\n" + noscript );
 }
 
 // process an svg file from the source directory
 function processFile(){
-  var theFile = files[ currfile ];
+	var theFile = files[ currfile ];
 
-  if( theFile ){
-    // only parse svg files
-    if( theFile.match( /\.svg$/i ) ){
-      (function(){
-        var page = require( "webpage" ).create();
-        var svgdata = fs.read(  inputdir + theFile ) || "";
-        var svgdatauri = "data:image/svg+xml;base64,";
-        var pngdatauri = "data:image/png;base64,";
+	if( theFile ){
+		// only parse svg files
+		if( theFile.match( /\.svg$/i ) ){
+			(function(){
+				var page = require( "webpage" ).create();
+				var svgdata = fs.read(  inputdir + theFile ) || "";
+				var svgdatauri = "data:image/svg+xml;base64,";
+				var pngdatauri = "data:image/png;base64,";
 
-        // kill the ".svg" at the end of the filename
-        var filenamenoext = theFile.replace( /\.svg$/i, "" );
+				// kill the ".svg" at the end of the filename
+				var filenamenoext = theFile.replace( /\.svg$/i, "" );
 
-        // get svg element's dimensions so we can set the viewport dims later
-        var frag = window.document.createElement( "div" );
-        frag.innerHTML = svgdata;
-        var svgelem = frag.querySelector( "svg" );
-        var width = svgelem.getAttribute( "width" );
-        var height = svgelem.getAttribute( "height" );
+				// get svg element's dimensions so we can set the viewport dims later
+				var frag = window.document.createElement( "div" );
+				frag.innerHTML = svgdata;
+				var svgelem = frag.querySelector( "svg" );
+				var width = svgelem.getAttribute( "width" );
+				var height = svgelem.getAttribute( "height" );
 
-        // get base64 of svg file
-        svgdatauri += btoa(svgdata);
+				// get base64 of svg file
+				svgdatauri += btoa(svgdata);
 
-        // add rules to svg data css file
-        datacssrules.push( "." + cssprefix + filenamenoext + " { background-image: url(" + svgdatauri + "); background-repeat: no-repeat; }" );
+				// add rules to svg data css file
+				datacssrules.push( "." + cssprefix + filenamenoext + " { background-image: url(" + svgdatauri + "); background-repeat: no-repeat; }" );
 
-        // add rules to png url css file
-        pngcssrules.push( "." + cssprefix + filenamenoext + " { background-image: url(" + pngout + filenamenoext + ".png" + "); background-repeat: no-repeat; }" );
-        
-        // add markup to the preview html file
-        htmlpreviewbody.push( '<pre><code>.' + cssprefix + filenamenoext + ':</code></pre><div class="' + cssprefix + filenamenoext + '" style="width: '+ width +'; height: '+ height +'"></div><hr/>' );
+				// add rules to png url css file
+				pngcssrules.push( "." + cssprefix + filenamenoext + " { background-image: url(" + pngout + filenamenoext + ".png" + "); background-repeat: no-repeat; }" );
 
-        // set page viewport size to svg dimensions
-        page.viewportSize = {  width: parseFloat(width), height: parseFloat(height) };
+				// add markup to the preview html file
+				htmlpreviewbody.push( '<pre><code>.' + cssprefix + filenamenoext + ':</code></pre><div class="' + cssprefix + filenamenoext + '" style="width: '+ width +'; height: '+ height +'"></div><hr/>' );
 
-        // open svg file in webkit to make a png
-        page.open(  inputdir + theFile, function( status ){
+				// set page viewport size to svg dimensions
+				page.viewportSize = {  width: parseFloat(width), height: parseFloat(height) };
 
-          // create png file
-          page.render( outputdir + pngout + filenamenoext + ".png" );
+				// open svg file in webkit to make a png
+				page.open(  inputdir + theFile, function( status ){
 
-          // create png data URI
-          pngdatacssrules.push( "." + cssprefix + filenamenoext + " { background-image: url(" +  pngdatauri + page.renderBase64( "png" ) + "); background-repeat: no-repeat; }" );
+					// create png file
+					page.render( outputdir + pngout + filenamenoext + ".png" );
 
-          // process the next svg
-          nextFile();
-        } );
-      }());
-    }
-    else {
-      // process the next svg
-      nextFile();
-    }
-  }
-  else {
-    // fin
-    finishUp();
-    phantom.exit();
-  }
+					// create png data URI
+					pngdatacssrules.push( "." + cssprefix + filenamenoext + " { background-image: url(" +  pngdatauri + page.renderBase64( "png" ) + "); background-repeat: no-repeat; }" );
+
+					// process the next svg
+					nextFile();
+				} );
+			}());
+		}
+		else {
+			// process the next svg
+			nextFile();
+		}
+	}
+	else {
+		// fin
+		finishUp();
+		phantom.exit();
+	}
 }
 
 // go ahead with the first file
