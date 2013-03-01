a004f342bfcc7cbf78c91961ecd74bfa8cd91a3c
Jeffrey Lembeck <jlembeck@gmail.com>
2013-02-28 20:03:15 -0800

    Moved all phantom passed constants in as an options hash. This brings us closer to testing

--- tasks/grunticon/phantom.js
+++ tasks/grunticon/phantom.js
@@ -8,7 +8,6 @@
 
 /*global phantom:true*/
 /*global window:true*/
-/*global btoa:true*/
 /*global require:true*/
 
 /*
@@ -33,24 +32,25 @@ phantom args sent from grunticon.js:
 	var img_stats = require('../../lib/img-stats');
 	var RSVP = require('../../lib/rsvp');
 
-	var inputdir = phantom.args[0];
-	var outputdir = phantom.args[1];
-	var pngout =  phantom.args[8];
-	var cssprefix = phantom.args[9];
-	var files = fs.list( inputdir );
-	var currfile = 0;
-	var pngcssrules = [];
-	var pngdatacssrules = [];
-	var datacssrules = [];
-	var htmlpreviewbody = [];
-	var fallbackcss = phantom.args[6];
-	var pngdatacss = phantom.args[5];
-	var datacss = phantom.args[4];
-	var cssbasepath = phantom.args[10];
+	var options = {
+		inputdir: phantom.args[0],
+		outputdir: phantom.args[1],
+		pngout:  phantom.args[8],
+		cssprefix: phantom.args[9],
+		fallbackcss: phantom.args[6],
+		pngdatacss: phantom.args[5],
+		datacss: phantom.args[4],
+		cssbasepath: phantom.args[10]
+	};
+
+	var pngcssrules = [],
+		pngdatacssrules = [],
+		datacssrules = [],
+		htmlpreviewbody = [];
 
 
 	// files have all been processed. write the css and html files and return
-	function finishUp(){
+	var finishUp = function( o ){
 
 		// make the preview HTML file and asyncCSS loader file
 		var asyncCSS = fs.read( phantom.args[2] );
@@ -62,14 +62,14 @@ phantom args sent from grunticon.js:
 		var htmldoc = fs.read( phantom.args[3]);
 
 		// noscript for the snippet file
-		var noscript = '<noscript><link href="' + cssbasepath + outputdir + fallbackcss + '" rel="stylesheet"></noscript>';
+		var noscript = '<noscript><link href="' + o.cssbasepath + o.outputdir + o.fallbackcss + '" rel="stylesheet"></noscript>';
 
 		// noscript for the preview file
-		var noscriptpreview = '<noscript><link href="' + fallbackcss + '" rel="stylesheet"></noscript>';
+		var noscriptpreview = '<noscript><link href="' + o.fallbackcss + '" rel="stylesheet"></noscript>';
 
 		// add custom function call to asyncCSS
-		asyncCSS += '\ngrunticon( [ "' + cssbasepath + outputdir + datacss +'", "' + cssbasepath + outputdir + pngdatacss +'", "' + cssbasepath + outputdir + fallbackcss +'" ] );';
-		asyncCSSpreview += '\ngrunticon( [ "'+ datacss +'", "'+ pngdatacss +'", "'+ fallbackcss +'" ] );';
+		asyncCSS += '\ngrunticon( [ "' + o.cssbasepath + o.outputdir + o.datacss +'", "' + o.cssbasepath + o.outputdir + o.pngdatacss +'", "' + o.cssbasepath + o.outputdir + o.fallbackcss +'" ] );';
+		asyncCSSpreview += '\ngrunticon( [ "'+ o.datacss +'", "'+ o.pngdatacss +'", "'+ o.fallbackcss +'" ] );';
 
 		// add async loader to the top
 		htmldoc = htmldoc.replace( /<script>/, "<script>\n\t" + asyncCSSpreview );
@@ -81,19 +81,19 @@ phantom args sent from grunticon.js:
 		htmldoc = htmldoc.replace( /<\/body>/, htmlpreviewbody.join( "\n\t" ) + "\n</body>" );
 
 		// write the preview html file
-		fs.write( outputdir + phantom.args[7], htmldoc );
+		fs.write( o.outputdir + phantom.args[7], htmldoc );
 
 		// write CSS files
-		fs.write( outputdir + fallbackcss, pngcssrules.join( "\n\n" ) );
-		fs.write( outputdir + pngdatacss, pngdatacssrules.join( "\n\n" ) );
-		fs.write( outputdir + datacss, datacssrules.join( "\n\n" ) );
+		fs.write( o.outputdir + o.fallbackcss, pngcssrules.join( "\n\n" ) );
+		fs.write( o.outputdir + o.pngdatacss, pngdatacssrules.join( "\n\n" ) );
+		fs.write( o.outputdir + o.datacss, datacssrules.join( "\n\n" ) );
 
 		// overwrite the snippet HTML
 		fs.write( phantom.args[2], "<!-- Unicode CSS Loader: place this in the head of your page -->\n<script>\n" + asyncCSS + "</script>\n" + noscript );
-	}
+	};
 
 	// process an svg file from the source directory
-	var processFile = function( theFile ){
+	var processFile = function( theFile , o ){
 		var promise = new RSVP.Promise();
 
 			var svgRegex = /\.svg$/i,
@@ -104,9 +104,8 @@ phantom args sent from grunticon.js:
 			if( isSvg || isPng ){
 				(function(){
 					var page = require( "webpage" ).create();
-					var imagedata = fs.read(  inputdir + theFile ) || "";
+					var imagedata = fs.read(  o.inputdir + theFile ) || "";
 					var svgdatauri = "'data:image/svg+xml;charset=US-ASCII,";
-					var pngdatauri = "'data:image/png;base64,";
 
 					// kill the ".svg" or ".png" at the end of the filename
 					var filenamenoext = theFile.replace( isSvg ? svgRegex : pngRegex, "" );
@@ -126,25 +125,14 @@ phantom args sent from grunticon.js:
 								"'";
 						}; //buildSVGDataURI
 
-						var prefix = cssprefix + filenamenoext;
+						var prefix = o.cssprefix + filenamenoext;
 
 						if( isSvg ) {
 							svgdatauri += buildSVGDataURI( imagedata );
 						}
 
-						var pngrule = '.' + prefix + " { background-image: url(" + pngout + filenamenoext + ".png" + "); background-repeat: no-repeat; }";
+						var pngrule = '.' + prefix + " { background-image: url(" + o.pngout + filenamenoext + ".png" + "); background-repeat: no-repeat; }";
 
-						var getPNGDataCSSRule = function( prefix , pngimgstring ){
-							if (pngimgstring.length <= 32768) {
-								// create png data URI
-								return "." + prefix + " { background-image: url(" +  pngdatauri + "); background-repeat: no-repeat; }";
-							} else {
-								return "/* Using an external URL reference because this image would have a data URI of " +
-									pngimgstring.length +
-									" characters, which is greater than the maximum of 32768 allowed by IE8. */\n" +
-									"." + prefix + " { background-image: url(" + pngout + filenamenoext + ".png" + "); background-repeat: no-repeat; }";
-							}
-						}; //getPNGDataCSSRule
 
 						// add rules to png url css file
 						pngcssrules.push( pngrule );
@@ -156,26 +144,42 @@ phantom args sent from grunticon.js:
 						page.viewportSize = {  width: parseFloat(width), height: parseFloat(height) };
 
 						// open svg file in webkit to make a png
-						page.open(  inputdir + theFile, function( status ){
+						page.open(  o.inputdir + theFile, function( status ){
+							if( status === "success" ){
+								var pngdatauri = "'data:image/png;base64,";
+								var pngimgstring = page.renderBase64( "png" );
+								var getPNGDataCSSRule = function( prefix , pngimgstring ){
+									if (pngimgstring.length <= 32768) {
+										// create png data URI
+										return "." + prefix + " { background-image: url(" +  pngdatauri + "); background-repeat: no-repeat; }";
+									} else {
+										return "/* Using an external URL reference because this image would have a data URI of " +
+											pngimgstring.length +
+											" characters, which is greater than the maximum of 32768 allowed by IE8. */\n" +
+											"." + prefix + " { background-image: url(" + o.pngout + filenamenoext + ".png" + "); background-repeat: no-repeat; }";
+									}
+								}; //getPNGDataCSSRule
 
-							var pngimgstring = page.renderBase64( "png" );
+								pngdatauri += pngimgstring + "'";
 
-							pngdatauri += pngimgstring + "'";
+								// create png file
+								page.render( o.outputdir + o.pngout + filenamenoext + ".png" );
 
-							// create png file
-							page.render( outputdir + pngout + filenamenoext + ".png" );
+								// add rules to svg data css file
+								datacssrules.push( "." + prefix + " { background-image: url(" + ( isSvg ? svgdatauri : pngdatauri ) + "); background-repeat: no-repeat; }" );
 
-							// add rules to svg data css file
-							datacssrules.push( "." + prefix + " { background-image: url(" + ( isSvg ? svgdatauri : pngdatauri ) + "); background-repeat: no-repeat; }" );
+								pngdatacssrules.push( getPNGDataCSSRule( prefix , pngimgstring ) );
 
-							pngdatacssrules.push( getPNGDataCSSRule( prefix , pngimgstring ) );
-
-							// process the next svg
-							promise.resolve();
+								// process the next svg
+								promise.resolve();
+							} else {
+								promise.reject();
+							}
 						} ); //page.open
 					}; // render
 
 
+					//TODO add SVG support to img_stats
 					var getStats = function( imagedata , callback ){
 						var width;
 						var height;
@@ -188,7 +192,7 @@ phantom args sent from grunticon.js:
 							height = svgelem.getAttribute( "height" );
 							callback( width , height );
 						} else {
-							img_stats.stats( inputdir + theFile , function( data ){
+							img_stats.stats( o.inputdir + theFile , function( data ){
 								width = data.width + 'px';
 								height = data.height + 'px';
 								callback( width , height );
@@ -212,14 +216,15 @@ phantom args sent from grunticon.js:
 		// create css and png rules
 		// render a png
 	// write files
-	//
+	
+	var files = fs.list( options.inputdir );
 	var promises = [];
 	files.forEach( function( file ){
-		promises.push( processFile( file ) );
+		promises.push( processFile( file , options ) );
 	});
 
 	RSVP.all( promises ).then( function(){
-		finishUp();
+		finishUp( options );
 		phantom.exit();
 	});
 })();
