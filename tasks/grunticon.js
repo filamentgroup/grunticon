19561eaaf08d99237324ee29ad54b45741ecafe7
Mat Marquis <mat@matmarquis.com>
2013-03-15 17:25:07 -0400

    Allows you to specify custom selectors on a per-file basis.

--- tasks/grunticon.js
+++ tasks/grunticon.js
@@ -61,6 +61,8 @@ module.exports = function( grunt , undefined ) {
 		// css references base path for the loader
 		var cssbasepath = config.cssbasepath || "/";
 
+		var customselectors = JSON.stringify( config.customselectors );
+
 		// folder name (within the output folder) for generated png files
 		var pngfolder = config.pngfolder || "png/";
 		// make sure pngfolder has / at the end
@@ -90,7 +92,6 @@ module.exports = function( grunt , undefined ) {
 
 		// take it to phantomjs to do the rest
 		grunt.log.write( "\ngrunticon now spawning phantomjs..." );
-
 		grunt.util.spawn({
 			cmd: 'phantomjs',
 			args: [
@@ -105,7 +106,8 @@ module.exports = function( grunt , undefined ) {
 				previewhtml,
 				pngfolder,
 				cssprefix,
-				cssbasepath
+				cssbasepath,
+				customselectors
 			],
 			fallback: ''
 		}, function(err, result, code) {
