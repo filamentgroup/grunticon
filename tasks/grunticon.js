1564e6cbdc939d1d2d5373818ca4f204eaa29192
Jeffrey Lembeck <jlembeck@gmail.com>
2013-07-23 11:41:39 -0700

    Adding optimization via SVGO

--- tasks/grunticon.js
+++ tasks/grunticon.js
@@ -13,6 +13,70 @@ module.exports = function( grunt , undefined ) {
 	"use strict";
 
 	var uglify = require( 'uglify-js' );
+	var svgo = new (require( 'svgo' ))();
+	var fs = require( 'fs' );
+
+	var RSVP = require( '../lib/rsvp' );
+
+	var readDir = function( path ){
+		var promise = new RSVP.Promise();
+		fs.readdir( path , function( err , files ){
+			if( err ){
+				grunt.log.error( err );
+				promise.reject( err );
+			} else {
+				promise.resolve( files );
+			}
+		});
+		return promise;
+	};
+
+	var readFile = function( filepath ){
+		var promise = new RSVP.Promise();
+		fs.readFile( filepath , function( err , data ){
+			if( err ){
+				grunt.log.error( err );
+				promise.reject( err );
+			} else {
+				promise.resolve( data );
+			}
+		});
+		return promise;
+	};
+
+	var optimize = function( file , data ){
+		var promise = new RSVP.Promise();
+		try{
+			if( file.match( /png/ ) ){
+				promise.resolve( data );
+			} else {
+				svgo.optimize( data.toString() , function( result ){
+					if( result.error ){
+						grunt.log.error( result.error );
+						promise.reject( result.error );
+					} else {
+						promise.resolve( result.data );
+					}
+				});
+			}
+		} catch(e) {
+			promise.reject( e );
+		}
+		return promise;
+	};
+
+	var writeFile = function( filepath , result ){
+		var promise = new RSVP.Promise();
+		fs.writeFile( filepath , result , function( err ){
+			if( err ){
+				grunt.log.error( err );
+				promise.reject( err );
+			} else {
+				promise.resolve();
+			}
+		});
+		return promise;
+	};
 
 	grunt.registerMultiTask( 'grunticon', 'A mystical CSS icon solution.', function() {
 		var done = this.async();
@@ -32,7 +96,7 @@ module.exports = function( grunt , undefined ) {
 		// fail if config or no src or dest config
 		if( !config || config.src === undefined || config.dest === undefined ){
 			grunt.fatal( "Oops! Please provide grunticon configuration for src and dest in your grunt.js file" );
-			return;
+			done( false );
 		}
 
 		// make sure src and dest have / at the end
@@ -89,54 +153,111 @@ module.exports = function( grunt , undefined ) {
 		// get color variables from config
 		var colors = JSON.stringify( config.colors || {} );
 
-		// create the output directory
-		grunt.file.mkdir( config.dest );
+		var svgosrc = config.src;
+		var tmp = __dirname + "/tmp/";
 
-		// create the output icons directory
-		grunt.file.mkdir( config.dest + pngfolder );
 
-		// minify the source of the grunticon loader and write that to the output
-		grunt.log.write( "\ngrunticon now minifying the stylesheet loader source." );
-		var banner = grunt.file.read( asyncCSSBanner );
-		var min = banner + "\n" + uglify.minify( asyncCSS ).code;
-		var loaderCodeDest = config.dest + loadersnippet;
-		grunt.file.write( loaderCodeDest, min );
-		grunt.log.write( "\ngrunticon loader file created." );
+		// create temp directory
+		grunt.log.write( "creating temp directory at:" + tmp );
+		grunt.file.mkdir( tmp );
 
-		// take it to phantomjs to do the rest
-		grunt.log.write( "\ngrunticon now spawning phantomjs..." );
-		var phantomJsPath = require('phantomjs').path;
-		grunt.log.write('(using path: ' + phantomJsPath + ')');
+		grunt.log.write( "\nUsing SVGO to optimize.\n" );
 
-		grunt.util.spawn({
-			cmd: phantomJsPath,
-			args: [
-				config.files.phantom,
-				config.src,
-				config.dest,
-				loaderCodeDest,
-				previewHTMLsrc,
-				datasvgcss,
-				datapngcss,
-				urlpngcss,
-				previewhtml,
-				pngfolder,
-				cssprefix,
-				cssbasepath,
-				customselectors,
-				width,
-				height,
-				colors
-			],
-			fallback: ''
-		}, function(err, result, code) {
-			// TODO boost this up a bit.
-			if( err ){
-				grunt.log.write("\nSomething went wrong with phantomjs...");
-				grunt.log.write( result.stderr );
-			}
-				grunt.log.write( result.stdout );
-			done();
+		readDir( svgosrc )
+		.then( function( files ){
+			var promise = new RSVP.Promise();
+			var promises = [];
+			files.forEach( function( file ){
+				var p;
+				if( file.match( /svg|png/ ) ){
+					p = new RSVP.Promise();
+
+					readFile( svgosrc + file )
+					.then( function( data , err ){
+						if( err ){
+							grunt.fatal( err );
+							done( false );
+						}
+						return optimize( file , data );
+					})
+					.then( function( result , err ){
+						if( err ){
+							grunt.fatal( err );
+							done( false );
+						}
+						return writeFile( tmp + file , result );
+					})
+					.then( function( _null , err ){
+						if( err ){
+							grunt.fatal( err );
+							done( false );
+						} else {
+							p.resolve();
+						}
+					});
+					promises.push( p );
+				}
+			});
+			RSVP.all( promises ).then( function(){
+				promise.resolve();
+			});
+			return promise;
+		})
+		.then( function(){
+			// create the output directory
+			grunt.file.mkdir( config.dest );
+
+			// create the output icons directory
+			grunt.file.mkdir( config.dest + pngfolder );
+
+
+			// minify the source of the grunticon loader and write that to the output
+			grunt.log.write( "\ngrunticon now minifying the stylesheet loader source." );
+			var banner = grunt.file.read( asyncCSSBanner );
+			var min = banner + "\n" + uglify.minify( asyncCSS ).code;
+			var loaderCodeDest = config.dest + loadersnippet;
+			grunt.file.write( loaderCodeDest, min );
+			grunt.log.write( "\ngrunticon loader file created." );
+
+			// take it to phantomjs to do the rest
+			grunt.log.write( "\ngrunticon now spawning phantomjs..." );
+			var phantomJsPath = require('phantomjs').path;
+			grunt.log.write('(using path: ' + phantomJsPath + ')');
+
+
+			grunt.util.spawn({
+				cmd: phantomJsPath,
+				args: [
+					config.files.phantom,
+					tmp,
+					config.dest,
+					loaderCodeDest,
+					previewHTMLsrc,
+					datasvgcss,
+					datapngcss,
+					urlpngcss,
+					previewhtml,
+					pngfolder,
+					cssprefix,
+					cssbasepath,
+					customselectors,
+					width,
+					height,
+					colors
+				],
+				fallback: ''
+			}, function(err, result, code) {
+				// TODO boost this up a bit.
+				if( err ){
+					grunt.log.write("\nSomething went wrong with phantomjs...");
+					grunt.fatal( result.stderr );
+					done( false );
+				} else {
+					grunt.log.write( result.stdout );
+					grunt.file.delete( tmp );
+					done();
+				}
+			});
 		});
 	});
 };
