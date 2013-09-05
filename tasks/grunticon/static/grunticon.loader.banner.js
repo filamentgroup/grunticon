34ba9ad421717216627db6a1eaa3ee62b392bc94
Mike Meyer <mikemeyer@gmail.com>
2013-09-05 15:29:53 -0700

    Happy new year.

--- tasks/grunticon/static/grunticon.loader.banner.js
+++ tasks/grunticon/static/grunticon.loader.banner.js
@@ -1,1 +1,1 @@
-/* grunticon Stylesheet Loader | https://github.com/filamentgroup/grunticon | (c) 2012 Scott Jehl, Filament Group, Inc. | MIT license. */
+/* grunticon Stylesheet Loader | https://github.com/filamentgroup/grunticon | (c) 2013 Scott Jehl, Filament Group, Inc. | MIT license. */
