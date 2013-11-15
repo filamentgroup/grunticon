91990684cfb7ee7d1db27a93942e1974206d86e0
John Bender <john.m.bender@gmail.com>
2013-11-15 12:36:56 -0800

    handle svg properly

--- test/directory-encoder.js
+++ test/directory-encoder.js
@@ -3,25 +3,28 @@ var constructor = require( path.join('..', 'lib', 'directory-encoder'));
 var fs = require('fs');
 
 "use strict";
-var encoder;
+var encoder, output = "test/output/encoded.css";
 
 
 exports['encode'] = {
 	setUp: function( done ) {
-		encoder = new constructor( "test/encoding", "test/output/encoded.css" );
+		encoder = new constructor( "test/encoding", output );
 		done();
 	},
 
 	output: function( test ) {
 		encoder.encode();
-		test.ok( fs.existsSync("test/output/encoded.css") );
+		test.ok( fs.existsSync(output) );
+		test.ok( /\.bear/.test(fs.readFileSync(output, {encoding: "utf8"})) );
 		test.done();
 	},
 
 	selector: function( test ) {
 		// TODO differentiate between same name files
-		encoder._css = function( name ){
+		encoder._css = function( name, data ){
 			test.equal( name, "bear" );
+
+			return constructor.prototype._css(name, data);
 		};
 
 		encoder.encode();
