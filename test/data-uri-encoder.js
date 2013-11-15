a6ef704788af52fa36e1320f1e1b6fb10495ecc6
John Bender <john.m.bender@gmail.com>
2013-11-15 11:09:42 -0800

    simple export

--- test/data-uri-encoder.js
+++ test/data-uri-encoder.js
@@ -1,5 +1,5 @@
 var path = require( 'path' );
-var constructor = require( path.join('..', 'lib', 'data-uri-encoder')).DataURIEncoder;
+var constructor = require( path.join('..', 'lib', 'data-uri-encoder'));
 var fs = require('fs');
 
 "use strict";
