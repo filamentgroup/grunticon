d127962ba848669b04aba4ac20822765c57c620a
John Bender <john.m.bender@gmail.com>
2013-11-15 10:46:06 -0800

    starting in on components

---
+++ test/data-uri-encoder.js
@@ -0,0 +1,34 @@
+var path = require( 'path' );
+var constructor = require( path.join('..', 'lib', 'data-uri-encoder')).DataURIEncoder;
+var fs = require('fs');
+
+"use strict";
+var encoder;
+
+exports['constructor'] = {
+	setUp: function( done ) {
+		encoder = new constructor( "test/files/bear.svg" );
+		done();
+	},
+
+	path: function( test ) {
+		test.equal( encoder.path, "test/files/bear.svg" );
+		test.done();
+	},
+
+	extension: function( test ) {
+		test.equal( encoder.extension, "svg" );
+		test.done();
+	},
+
+	prefix: function( test ) {
+		test.equal( encoder.prefix, "data:image/svg+xml;charset=US-ASCII," );
+		test.done();
+	},
+
+	prefixOverride: function( test ) {
+		encoder = new constructor( "test/files/bear.svg", "foo" );
+		test.equal( encoder.prefix, "foo" );
+		test.done();
+	}
+};
