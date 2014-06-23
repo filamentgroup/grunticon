e3f812eb09708b2770d0a3c3bbb07337c36284a7
Scott Jehl <scottjehl@gmail.com>
2014-06-23 15:29:34 -0500

    Update grunticon loader with async guarantee from loadCSS: from https://github.com/filamentgroup/loadCSS

--- tasks/grunticon/static/grunticon.loader.js
+++ tasks/grunticon/static/grunticon.loader.js
@@ -7,15 +7,20 @@ window.grunticon = function( css, foo ){
 	// Thanks Modernizr & Erik Dahlstrom
 	var w = window,
 		svg = !!w.document.createElementNS && !!w.document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect && !!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1") && !(window.opera && navigator.userAgent.indexOf('Chrome') === -1),
-
+		/*! loadCSS: borrowed from https://github.com/filamentgroup/loadCSS */
 		loadCSS = function( data ){
 			var link = w.document.createElement( "link" ),
 				ref = w.document.getElementsByTagName( "script" )[ 0 ];
 			link.rel = "stylesheet";
 			link.href = css[ data && svg ? 0 : data ? 1 : 2 ];
+			// temporarily, set media to something non-matching to ensure it'll fetch without blocking render
+			link.media = "only x";
 			ref.parentNode.insertBefore( link, ref );
+			// set media back to `all` so that the styleshet applies once it loads
+			setTimeout( function(){
+				link.media = "all";
+			} );
 		},
-
 		// Thanks Modernizr
 		img = new w.Image();
 
