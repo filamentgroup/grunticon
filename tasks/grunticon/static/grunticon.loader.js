f0e01922bcff748595dc68d43141013d5e6a1a9b
Jeffrey Lembeck <jlembeck@gmail.com>
2014-12-29 14:44:19 -0800

    Use 'grunticon:' instead of 'foo'

--- tasks/grunticon/static/grunticon.loader.js
+++ tasks/grunticon/static/grunticon.loader.js
@@ -5,7 +5,7 @@
 		navigator = window.navigator,
 		Image = window.Image;
 	
-	var selectorPlaceholder = "foo";
+	var selectorPlaceholder = "grunticon:";
 
 	// Thanks Modernizr & Erik Dahlstrom
 	var svg = !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect && !!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1") && !(window.opera && navigator.userAgent.indexOf('Chrome') === -1) && navigator.userAgent.indexOf('Series40') === -1;
