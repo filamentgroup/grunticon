88b65b174a1aae2cc062a5c7b5f671d09d395e0c
Scott Jehl <scottjehl@gmail.com>
2015-01-28 14:34:29 -0600

    starting to put together an explainer site. This one has some text starting points, a logo, and an animated example icon. weeeee!

---
+++ site/gruntfile.js
@@ -0,0 +1,75 @@
+/*
+ * grunticon
+ * https://github.com/filamentgroup/grunticon
+ *
+ * Copyright (c) 2012 Scott Jehl
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+	"use strict";
+
+	// Project configuration.
+	grunt.initConfig({
+		watch: {
+			files: '<config:lint.files>',
+			tasks: 'default'
+		},
+		svgmin: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'src/svg',
+					src: ['*.svg'],
+					dest: 'src/svg'
+				}]
+			}
+		},
+		grunticon: {
+			foo: {
+				files: [{
+					expand: true,
+					cwd: 'src/svg',
+					src: ['*.svg', '*.png'],
+					dest: "grunticon/"
+				}],
+				options: {
+
+					// CSS filenames
+					datasvgcss: "icons.data.svg.css",
+					datapngcss: "icons.data.png.css",
+					urlpngcss: "icons.fallback.css",
+
+					// grunticon loader code snippet filename
+					loadersnippet: "grunticon.loader.js",
+
+					// folder name (within dest) for png output
+					pngfolder: "png",
+
+					// prefix for CSS classnames
+					cssprefix: ".icon-",
+
+
+					// css file path prefix - this defaults to "/" and will be placed before the "dest" path when stylesheets are loaded.
+					// This allows root-relative referencing of the CSS. If you don't want a prefix path, set to to ""
+					cssbasepath: "/",
+					enhanceSVG: true,
+					customselectors: {
+						"close" : [".icon-plus.toggled"]
+					}
+
+				}
+			}
+		}
+	});
+
+	// Load local tasks.
+
+	grunt.loadNpmTasks( 'grunt-svgmin' );
+	grunt.loadNpmTasks( 'grunt-grunticon' );
+
+	// Default task.
+	grunt.registerTask('default', [ 'grunticon']);
+
+};
+
