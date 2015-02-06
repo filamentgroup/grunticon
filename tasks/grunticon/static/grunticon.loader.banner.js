8eee7d9e727cef1d3711ae58dc0c230806cea2a6
Jeffrey Lembeck <jlembeck@gmail.com>
2015-02-06 13:16:06 -0800

    Rev version to 2.1.2

--- tasks/grunticon/static/grunticon.loader.banner.js
+++ tasks/grunticon/static/grunticon.loader.banner.js
@@ -1,1 +1,1 @@
-/*! grunt-grunticon Stylesheet Loader - v2.1.1 | https://github.com/filamentgroup/grunticon | (c) 2015 Scott Jehl, Filament Group, Inc. | MIT license. */
+/*! grunt-grunticon Stylesheet Loader - v2.1.2 | https://github.com/filamentgroup/grunticon | (c) 2015 Scott Jehl, Filament Group, Inc. | MIT license. */
